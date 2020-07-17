010f6aaea00cea77331880390fac2779127952ee
Piotr Bochynski <piotr.bochynski@sap.com>
2020-07-17 16:54:35 +0200

    Speed up apiserver-proxy installation

--- kyma-k3d.sh
+++ kyma-k3d.sh
@@ -1,22 +1,25 @@
 SECONDS=0
 
+function waitForJobs() {
+    while (( (( JOBS_COUNT=$(jobs -p | wc -l) )) > 0 )); do echo "Waiting for $JOBS_COUNT command(s) executed in the background, elapsed time: $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"; jobs >/dev/null ; sleep $1; done
+}
+
 # Create Kyma cluster
 k3d create --publish 80:80 --publish 443:443 --enable-registry --registry-volume local_registry --registry-name registry.localhost --server-arg --no-deploy --server-arg traefik -n kyma -t 60 
+
+
 
 # Delete cluster with keep-registry-volume to cache docker images
 # k3d delete --keep-registry-volume -n kyma
 echo "Cluster created in $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"
-export KUBECONFIG="$(k3d get-kubeconfig -n='kyma')"
-export DOMAIN=local.kyma.dev
-export GARDENER=false
-export OVERRIDES=global.isLocalEnv=true,global.ingress.domainName=$DOMAIN,global.environment.gardener=$GARDENER,global.domainName=$DOMAIN,global.minikubeIP=127.0.0.1,global.tlsCrt=ZHVtbXkK
-export ORY=global.ory.hydra.persistence.enabled=false,global.ory.hydra.persistence.postgresql.enabled=false,hydra.hydra.autoMigrate=false
-export LOCALREGISTRY="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,global.ingress.domainName=$DOMAIN"
-export REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /k3d-registry)
-sed "s/REGISTRY_IP/$REGISTRY_IP/" coredns-patch.tpl >coredns-patch.yaml
+KUBECONFIG="$(k3d get-kubeconfig -n='kyma')"
 
 # This file will be created by cert-manager (not needed anymore):
 rm resources/core/charts/gateway/templates/kyma-gateway-certs.yaml
+
+# apiserver-proxy dependencies are not required (cannot be disabled by values yet):
+rm resources/apiserver-proxy/requirements.yaml
+rm -R resources/apiserver-proxy/charts
 
 # Create namespaces
 kubectl create ns kyma-system
@@ -30,17 +33,26 @@ kubectl create ns natss
 kubectl label ns istio-system istio-injection=disabled --overwrite
 kubectl label ns cert-manager istio-injection=disabled --overwrite
 
-helm upgrade -i cluster-essentials resources/cluster-essentials --set $OVERRIDES -n kyma-system &
-helm upgrade -i testing resources/testing --set $OVERRIDES -n kyma-system &
+helm upgrade -i cluster-essentials resources/cluster-essentials -n kyma-system &
+helm upgrade -i testing resources/testing -n kyma-system &
 kubectl apply -f cert-manager.yaml &
+
+# Patch CoreDNS with entries for registry.localhost and *.local.kyma.dev
+REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /k3d-registry)
+sed "s/REGISTRY_IP/$REGISTRY_IP/" coredns-patch.tpl >coredns-patch.yaml
 kubectl -n kube-system patch cm coredns --patch "$(cat coredns-patch.yaml)" &
-helm upgrade -i istio resources/istio -n istio-system --set $OVERRIDES &
+helm upgrade -i istio resources/istio --set global.isLocalEnv=true -n istio-system &
 
 while [[ $(kubectl get pods -n istio-system -l istio=sidecar-injector -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for Istio sidecar-injector, elapsed time: $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"; sleep 10; done
 echo "Istio installed in $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"
 
+DOMAIN=local.kyma.dev
+OVERRIDES=global.isLocalEnv=false,global.ingress.domainName=$DOMAIN,global.environment.gardener=false,global.domainName=$DOMAIN,global.tlsCrt=ZHVtbXkK
+ORY=global.ory.hydra.persistence.enabled=false,global.ory.hydra.persistence.postgresql.enabled=false,hydra.hydra.autoMigrate=false
+LOCALREGISTRY="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,global.ingress.domainName=$DOMAIN"
+
 helm upgrade -i ingress-dns-cert ingress-dns-cert --set $OVERRIDES -n istio-system & 
-helm upgrade -i istio-kyma-patch resources/istio-kyma-patch -n istio-system --set $OVERRIDES &
+helm upgrade -i istio-kyma-patch resources/istio-kyma-patch -n istio-system &
 
 helm upgrade -i dex resources/dex --set $OVERRIDES -n kyma-system &
 helm upgrade -i ory resources/ory --set $OVERRIDES --set $ORY -n kyma-system &
@@ -71,7 +83,7 @@ helm upgrade -i event-sources resources/event-sources -n kyma-system &
 kubectl apply -f installer-local.yaml &
 
 # Wait for jobs - helm commands executed in the background
-while (( (( JOBS_COUNT=$(jobs -p | wc -l) )) > 0 )); do echo "Waiting for $JOBS_COUNT command(s) executed in the background, elapsed time: $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"; jobs >/dev/null ; sleep 10; done
+waitForJobs 10
 
 echo "##############################################################################"
 echo "# Kyma cluster created in $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"
