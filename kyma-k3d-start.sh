86487b3b1ff074ae20ddd29a76de3e38b749701f
Juergen Kleinowitz <jkleinowitz@gmail.com>
2020-09-18 14:01:59 +0200

    Update kyma-k3d-start.sh

--- kyma-k3d-start.sh
+++ kyma-k3d-start.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit          # Exit on most errors (see the manual)
+set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+set -o pipefail         # Use last non-zero exit code in a pipeline
+
 k3d cluster start kyma --wait
 export KUBECONFIG="$(k3d kubeconfig merge kyma --switch-context)"
 # Wait for nodes to be ready before scheduling any workload
