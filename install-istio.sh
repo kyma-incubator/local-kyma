2784aa3326abe218304cba58d550233a577aa1eb
pbochynski <p.bochynski@gmail.com>
2020-10-13 23:27:35 +0200

    Separate istio installation

---
+++ install-istio.sh
@@ -0,0 +1,5 @@
+# Instal istio
+if [[ ! -f istio-1.5.10/bin/istioctl ]]; then
+  curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=1.5.10 sh -
+fi
+istio-1.5.10/bin/istioctl manifest apply --set profile=demo 
