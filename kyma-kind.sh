38200ac0080b8f337163d0416c7b556ad2680c29
Daniel Roth <daniel.roth02@sap.com>
2020-10-14 12:24:30 -0700

    k3d and kind install parity

--- kyma-kind.sh
+++ kyma-kind.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-set -o errexit
-
-### Create kind cluster
-
 ./create-cluster-kind.sh
-
-while [[ $(kubectl get nodes -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for cluster nodes to be ready"; sleep 2; done
-
-### Download istioctl
-
-./download-istioctl.sh
-
-### Install istio 1.5.10-distroless
-./istioctl manifest apply -f kind-istio-install.yaml
-
-### Install kyma
-export SKIP_MODULES="apiserver-proxy,helm-broker,console,cluster-users,apiserver-proxy,logging,tracing"
+./install-istio.sh
 ./install-kyma.sh
