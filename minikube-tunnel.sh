b7adc578ee31843f3780b818d0fc769e80ca4b4f
pbochynski <p.bochynski@gmail.com>
2020-10-15 08:57:43 +0200

    Add minikube tunnel

---
+++ minikube-tunnel.sh
@@ -0,0 +1,4 @@
+minikube tunnel -c >/dev/null 2>&1 &
+LB_IP=""
+while [[ -z $LB_IP ]]; do LB_IP=$(kubectl get svc istio-ingressgateway -n istio-system -ojsonpath='{.status.loadBalancer.ingress[0].ip}'); echo "Waiting for LoadBalancer IP: $LB_IP"; sleep 5; done
+sh -c "echo \"$LB_IP commerce.local.kyma.dev dex.local.kyma.dev lastorder.local.kyma.dev\">>/etc/hosts"
