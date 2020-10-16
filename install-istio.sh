5f84c07131ae00d2a696cd6f5160b18661fdbdd2
pbochynski <p.bochynski@gmail.com>
2020-10-16 14:33:00 +0200

    Add istio configuration

--- install-istio.sh
+++ install-istio.sh
@@ -2,4 +2,4 @@
 if [[ ! -f istio-1.5.10/bin/istioctl ]]; then
   curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=1.5.10 sh -
 fi
-istio-1.5.10/bin/istioctl manifest apply --set profile=demo 
+istio-1.5.10/bin/istioctl manifest apply --set profile=demo $@
