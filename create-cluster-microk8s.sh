25675084ee2ae2321fa8432550ee8e757fffeffc
Daniel Roth <daniel.roth02@sap.com>
2020-10-21 13:14:18 -0700

    add local docker registry

--- create-cluster-microk8s.sh
+++ create-cluster-microk8s.sh
@@ -1,5 +1,14 @@
 SECONDS=0  
 
+# create registry container unless it already exists
+docker run -d \
+-p 5000:5000 \
+--restart=always \
+--network=kind \
+--name registry.localhost \
+-v $PWD/registry:/var/lib/registry \
+registry:2
+
 sudo microk8s enable dns
 
 sudo microk8s status --wait-ready
