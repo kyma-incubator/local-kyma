bef69df85b283576798ec04db4550c4f6a7475cd
Daniel Roth <daniel.roth02@sap.com>
2020-10-21 12:30:50 -0700

    write config in every step

--- create-cluster-microk8s.sh
+++ create-cluster-microk8s.sh
@@ -4,8 +4,4 @@ microk8s enable dns
 
 microk8s status --wait-ready
 
-mkdir -p ~/.kube
-
-microk8s config > ~/.kube/config
-
 echo "Cluster created in $(( $SECONDS/60 )) min $(( $SECONDS % 60 )) sec"
