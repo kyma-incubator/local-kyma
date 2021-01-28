91da0398c07460075ec88d87894b15ba3bcaff8c
pbochynski <p.bochynski@gmail.com>
2021-01-28 15:58:19 +0100

    Upgrade istioctl

--- install-istio.sh
+++ install-istio.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Instal istio
-if [[ ! -f istio-1.7.4/bin/istioctl ]]; then
-  curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=1.7.4 sh -
+if [[ ! -f istio-1.8.2/bin/istioctl ]]; then
+  curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=1.8.2 sh -
 fi
 istio-1.7.4/bin/istioctl install --set profile=demo $@
