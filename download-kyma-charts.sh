d30d576db86291eaa8c674a0a30e8237444b8eb8
pbochynski <p.bochynski@gmail.com>
2021-01-28 15:31:52 +0100

    Fix tests path

--- download-kyma-charts.sh
+++ download-kyma-charts.sh
@@ -4,6 +4,7 @@ curl -s https://codeload.github.com/${REPO}/zip/${BRANCH} --output kyma-src.zip
 unzip --qq -d ./tmp kyma-src.zip
 rm -rf ./resources
 mv ./tmp/*/resources ./
-rm -rf ./fast-integration
-mv ./tmp/*/tests/fast-integration ./
+rm -rf ./tests
+mkdir ./tests
+mv ./tmp/*/tests/fast-integration ./tests
 rm -rf ./tmp
