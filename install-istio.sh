470f76b034b2494271c619ab2d7028c963092b3d
pbochynski <p.bochynski@gmail.com>
2021-01-28 16:01:15 +0100

    Fix istio version

--- install-istio.sh
+++ install-istio.sh
@@ -5,4 +5,4 @@ set -e
 if [[ ! -f istio-1.8.2/bin/istioctl ]]; then
   curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=1.8.2 sh -
 fi
-istio-1.7.4/bin/istioctl install --set profile=demo $@
+istio-1.8.2/bin/istioctl install --set profile=demo $@
