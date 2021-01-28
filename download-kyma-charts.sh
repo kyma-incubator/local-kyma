ab20a7d4d61e44ba5460120e770d916adbbf6789
pbochynski <p.bochynski@gmail.com>
2021-01-28 14:17:18 +0100

    Update to latest istio and k3d

--- download-kyma-charts.sh
+++ download-kyma-charts.sh
@@ -4,5 +4,6 @@ curl -s https://codeload.github.com/${REPO}/zip/${BRANCH} --output kyma-src.zip
 unzip --qq -d ./tmp kyma-src.zip
 rm -rf ./resources
 mv ./tmp/*/resources ./
+rm -rf ./fast-integration
 mv ./tmp/*/tests/fast-integration ./
 rm -rf ./tmp
